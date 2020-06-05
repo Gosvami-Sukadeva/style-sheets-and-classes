b92b1f7fe2c720acc07465d5e02a6a996b24906f
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-06-05 19:05:34 +0200

    style sheets and classes

---
+++ src/Text.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Text.css";
+const Text = () => {
+  return (
+    <p className="red">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aut nobis
+      similique libero dicta distinctio fugiat magnam velit labore atque, vitae
+      voluptas itaque neque amet repellendus dolores ut necessitatibus
+      voluptates?
+    </p>
+  );
+};
+
+export default Text;
